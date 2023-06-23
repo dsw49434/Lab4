ad676cb3593c2b92f7c9918a9ffafd8668f81473
dsw49434 <dsw49434@student.dsw.edu.pl>
2023-06-23 14:25:28 +0200

    modified:   skrypt.sh

--- skrypt.sh
+++ skrypt.sh
@@ -8,3 +8,19 @@ do
   echo "Nazwa skryptu: $0" >> "$filename"
   echo "Data: $(date)" >> "$filename"
 done
+
+
+if [ "$#" -ne 1 ]; then
+  echo "Podaj liczbę plikow."
+  exit 1
+fi
+
+num_files=$1
+
+for ((i=1; i<=num_files; i++))
+do
+  filename="log${i}.txt"
+  echo "Nazwa pliku: $filename" > "$filename"
+  echo "Nazwa skryptu: $0" >> "$filename"
+  echo "Data: $(date)" >> "$filename"
+done
